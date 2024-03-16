050dea29eef5f044a3b2b30d9b672817d2394bf1
udit saurabh <uditsaurabh@udits-MacBook-Pro.local>
2024-03-16 10:19:25 +0530

    Initial Commit

---
+++ main.py
@@ -0,0 +1,8 @@
+def my_function():
+  # Your function logic here
+  print("This is my_function")
+
+if __name__ == '__main__':
+  # Initialize your code here (if needed)
+  print("Initializing...")
+  my_function()
