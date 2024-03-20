5d458523d1c538056a46cebdfd5f0174a4769cec
udit saurabh <uditsaurabh@udits-MacBook-Pro.local>
2024-03-20 06:04:15 +0530

    celery and redis added

--- authors_api/settings/base.py
+++ authors_api/settings/base.py
@@ -4,83 +4,84 @@ import os
 
 env = environ.Env()
 
-#Error Prone
+# Error Prone
 ROOT_DIR = Path(__file__).resolve().parent.parent.parent
 
-#Error Prone
-APPS_DIR = ROOT_DIR / 'core_apps'
+# Error Prone
+APPS_DIR = ROOT_DIR / "core_apps"
 
-DEBUG = env.bool("DJANGO_DEBUG",False)
+DEBUG = env.bool("DJANGO_DEBUG", False)
 
-#Secret key moved to localfile
-#ALLOWED_HOSTS = []
+# Secret key moved to localfile
+# ALLOWED_HOSTS = []
 
 
 # Application definition
 
 DJANGO_APPS = [
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.sites',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'django.contrib.admin',
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.sites",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "django.contrib.admin",
 ]
 
 THIRD_PARTY_APPS = [
-    'rest_framework',
-    'django_filters',
-    'django_countries',
-    'phonenumber_field',
-    'drf_yasg',
-    'corsheaders'
+    "rest_framework",
+    "django_filters",
+    "django_countries",
+    "phonenumber_field",
+    "drf_yasg",
+    "corsheaders",
+    "djcelery_email",
 ]
 
-LOCAL_APPS=['core_apps.common','core_apps.profiles','core_apps.users']
+LOCAL_APPS = ["core_apps.common", "core_apps.profiles", "core_apps.users"]
 
-INSTALLED_APPS = LOCAL_APPS + DJANGO_APPS +THIRD_PARTY_APPS
+INSTALLED_APPS = LOCAL_APPS + DJANGO_APPS + THIRD_PARTY_APPS
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
     #'django.middleware.common.BrokenLinkEmailMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = 'authors_api.urls'
+ROOT_URLCONF = "authors_api.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [str(APPS_DIR/"templates")],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.template.context_processors.i18n',
-                'django.template.context_processors.static',
-                'django.template.context_processors.tz',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [str(APPS_DIR / "templates")],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.template.context_processors.i18n",
+                "django.template.context_processors.static",
+                "django.template.context_processors.tz",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'authors_api.wsgi.application'
+WSGI_APPLICATION = "authors_api.wsgi.application"
 
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {"default":env.db("DATABASE_URL")}
+DATABASES = {"default": env.db("DATABASE_URL")}
 DATABASES["default"]["ATOMIC_REQUESTS"] = True
 
 PASSWORD_HASHERS = [
@@ -96,16 +97,16 @@ PASSWORD_HASHERS = [
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 
@@ -113,9 +114,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = 'Asia/Kolkata'
+TIME_ZONE = "Asia/Kolkata"
 
 USE_I18N = True
 
@@ -125,33 +126,45 @@ USE_TZ = True
 
 SITE_ID = 1
 
-ADMIN_URL = 'supersecret/'
+ADMIN_URL = "supersecret/"
 
-ADMINS = [("uditsaurabh","uditsaurabh@gmail.com")]
+ADMINS = [("uditsaurabh", "uditsaurabh@gmail.com")]
 
 MANAGERS = ADMINS
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/staticfiles/'
-STATIC_ROOT = str(ROOT_DIR/"staticfiles")
-STATICFILES_DIRS=[]
-STATICFILES_FINDERS=[
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+STATIC_URL = "/staticfiles/"
+STATIC_ROOT = str(ROOT_DIR / "staticfiles")
+STATICFILES_DIRS = []
+STATICFILES_FINDERS = [
+    "django.contrib.staticfiles.finders.FileSystemFinder",
+    "django.contrib.staticfiles.finders.AppDirectoriesFinder",
 ]
 
 MEDIA_URL = "/mediafiles/"
-MEDIA_ROOT = str(ROOT_DIR/"mediafiles")
+MEDIA_ROOT = str(ROOT_DIR / "mediafiles")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-CORS_URLS_REGEX=r"^/api/.*$"
+CORS_URLS_REGEX = r"^/api/.*$"
 
 AUTH_USER_MODEL = "users.User"
+
+CELERY_BROKER_URL = env("CELERY_BROKER")
+CELERY_RESULT_BACKEND = CELERY_BROKER_URL
+CELERY_ACCEPT_CONTENT = ["json"]
+CELERY_TASK_SERIALIZER = "json"
+CELERY_RESULT_SERIALIZER = "json"
+CELERY_RESULT_BACKEND_MAX_RETRIES = 10
+CELERY_TASK_SEND_SENT_EVENT = True
+
+if USE_TZ:
+    CELERY_TIMEZONE = TIME_ZONE
+
 
 LOGGING = {
     "version": 1,
@@ -168,5 +181,5 @@ LOGGING = {
             "formatter": "verbose",
         }
     },
-    "root": {"level": "INFO", "handlers": ["console"]}
+    "root": {"level": "INFO", "handlers": ["console"]},
 }
--- authors_api/settings/local.py
+++ authors_api/settings/local.py
@@ -2,7 +2,20 @@ from .base import *
 from .base import env
 
 
-DEBUG=True
+DEBUG = True
 
-SECRET_KEY =env("DJANGO_SECRET_KEY",default='django-insecure-tfu)o3zuc!=2l&&)70(*3f15b^%63-%pkxf^39ij$hu^)sqc)@')
-ALLOWED_HOSTS = ["localhost","0.0.0.0","127.0.0.1"]
+SECRET_KEY = env(
+    "DJANGO_SECRET_KEY",
+    default="django-insecure-tfu)o3zuc!=2l&&)70(*3f15b^%63-%pkxf^39ij$hu^)sqc)@",
+)
+ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1"]
+
+DEBUG = True
+CSRF_TRUSTED_ORIGINS = ["http://localhost:8080"]
+
+EMAIL_BACKEND = "djcelery_email.backends.CeleryEmailBackend"
+EMAIL_HOST = env("EMAIL_HOST", default="mailhog")
+EMAIL_PORT = env("EMAIL_PORT")
+DEFAULT_FROM_EMAIL = "support@apiimperfect.site"
+DOMAIN = env("DOMAIN")
+SITE_NAME = "Authors Havens"
